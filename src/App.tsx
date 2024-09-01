7a09cbc85054d893bc3897e41fc834dab1ca0f65
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-09-01 15:42:49 +0400

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-[#afsa1s] w-[551.5px]  flex flex-row gap-[">
+        <p className='text-white text-3xl'>ssssss</p>
+        <p className='text-white text-3xl text-center'>psas</p>
+      </div>
+      <p>123123123</p>
+    </>
+
+  )
+}
+
+export default App
