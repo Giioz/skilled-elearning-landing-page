3b57a2cae0a250d6352c16a175a9da9c4e324d82
Mamaladze Gio <mamaladzegio99@gmail.com>
2024-09-10 11:33:43 +0400

    feat : added icons/ fixed src paths

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -9,7 +9,7 @@ export const ContentCard: React.FC<Props> = ({ item }) => {
     <div className="pt-[24px] grid">
       <div className="max-w-[21.4rem] min-h-[16.1rem] px-2 bg-white rounded-[10px] pl-[28px]">
         <div className="max-w-[3.5rem] h-[3.5rem] customlinear rounded-full flex justify-center items-center translate-y-[-50%]">
-          <img className="max-w-[22px] h-[24px]" src={item.img} alt="" />
+          <img className="w-[22px] h-[22px]" src={item.img} alt="" />
         </div>
         <div className="font-heading">
           <p className="text-[20px] text-[#13183F] font-bold">{item.title}</p>
--- src/configs/ContentConfig.ts
+++ src/configs/ContentConfig.ts
@@ -9,35 +9,35 @@ export type IContent = {
 export const ContentConfig:IContent[] = [
     {
         id : 0,
-        img : '../../public/icons/Shape.svg',
+        img : './icons/Shape.png',
         title : 'Animation',
         description : 'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
         buttonText : 'Get Started'
     },
     {
         id : 1,
-        img : 'test',
+        img : './icons/Mask.png',
         title : 'Design',
         description : 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
         buttonText : 'Get Started'
     },
     {
         id : 2,
-        img : 'test',
+        img : './icons/Mask (1).png',
         title : 'Photography',
         description : 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
         buttonText : 'Get Started'
     },
     {
         id : 3,
-        img : 'test',
+        img : './icons/Mask (2).png',
         title : 'Crypto',
         description : 'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
         buttonText : 'Get Started'
     },
     {
         id : 4,
-        img : 'test',
+        img : './icons/Mask (3).png',
         title : 'Business',
         description : 'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
         buttonText : 'Get Started'
